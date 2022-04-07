7017ef9b5424d2cadf929607fd905f4dddf2d8aa
ELENIA30 <elenia.centamore30@gmail.com>
2022-04-07 21:19:42 +0200

    commit2

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
 // const Apikey = '8d19f834fee6f5ee5a0141a527dcc528 '  ;
 // const uri ='http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='8d19f834fee6f5ee5a0141a527dcc528';
 import {
-    createCard
+    createCard,
+    updateCard
 } from "./utils.js";
-
-const call = document.querySelector("#city");
-call.addEventListener('change', () => {
-    let city = call.value;
-    
-    console.log(city)
-    getAPIdata(city);
-});
-
-
+const getCitiesNames = () => {
+    const allCity = [];
+    const selectCities = document.getElementById('city');
+    for(let indice = 0; indice < selectCities.children.length; indice++) {
+        const valueOptionSelezionata = selectCities.children[indice].value;
+        allCity.push(valueOptionSelezionata);
+    }
+    return allCity;
+}
+const createCitiesCards = (cities) => {
+    cities.map((city) => {
+        getAPIdata(city).then((city) => {
+            createCard(
+                city.name,
+                city.weather[0].description,
+                city.weather[0].main,
+                city.weather[0].icon,
+            );
+        });
+    });
+};
 
 const getAPIdata = async (city) => {
-    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8d19f834fee6f5ee5a0141a527dcc528`)
+    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8d19f834fee6f5ee5a0141a527dcc528&lang=en`)
     const res = await api.json();
     return res
 }
-getAPIdata("roma").then((city) => {
-    console.log(city);
-    createCard(city.name, city.weather[0].description, city.weather[0].main, city.weather[0].icon);
+const call = document.querySelector("#city");
+const cities = getCitiesNames();
+call.addEventListener('change', () => {
+    let city = call.value;
 
+    if (city === 'tutte') {
+        createCitiesCards(cities);
+        return;
+    } 
 
+    getAPIdata(city).then((city) => {
+        updateCard(city.name, city.weather[0].description, city.weather[0].main, city.weather[0].icon); 
+    });
+}); 
 
-  
+getAPIdata(call.value).then((city) => {
+    if(city === 'tutte') return;
+    createCard(city.name, city.weather[0].description, city.weather[0].main, city.weather[0].icon); 
 });
+
+
+// const call = document.querySelector("#city");
+// getAPIdata(call.value).then((city) => {
+
+//     createCard(city.name, city.weather[0].description, city.weather[0].main, city.weather[0].icon);
+// })
+
+// call.addEventListener('change', () => {
+//     let city = call.value;
+//     getAPIdata(city).then((city) => {
+//         createCard(city.name, city.weather[0].description, city.weather[0].main, city.weather[0].icon);
+//         const card = document.querySelector(".card");
+//         document.querySelector(".cardsWrapper").removeChild(card);
+//     });
+// });
 
